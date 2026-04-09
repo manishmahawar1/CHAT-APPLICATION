9fe5e2e80c604321f2c25ae8d22a462c70f0b9df
Manish <khushikumari80005@gmal.com>
2026-04-09 22:23:56 +0530

    fix again

--- frontend/chatapp/src/Store/useAuthStore.js
+++ frontend/chatapp/src/Store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client"
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : "/"
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 
 const useAuthStore = create((set, get) => (
